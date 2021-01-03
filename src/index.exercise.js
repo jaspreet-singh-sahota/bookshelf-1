39eaa139a66746153b2cb7fd3e68484573ff5916
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2021-01-03 13:08:05 +0530

    Index exercise extar-1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Logo } from './components/logo'
+import { Dialog } from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
 const App = () => {
+  const [modelOpen, setModelOpen] = useState('none')
   return (
     <div>
       <Logo height={80} width={80}/>
       <h1>Bookshelf</h1>
-      <button onClick={() => alert('login was clicked')}>login</button>
-      <button onClick={() => alert('register was clicked')}>register</button>
+      <button onClick={() => setModelOpen('login')}>login</button>
+      <button onClick={() => setModelOpen('register')}>register</button>
+      <Dialog aria-label="Login form" isOpen={modelOpen === 'login'} >
+        <button onClick={() => setModelOpen('none')}>Close</button>
+        <h1>login</h1>
+      </Dialog>
+      <Dialog aria-label="register form" isOpen={modelOpen === 'register'} >
+        <button onClick={() => setModelOpen('none')}>Close</button>
+        <h1>login</h1>
+      </Dialog>
     </div>
   )
 }
